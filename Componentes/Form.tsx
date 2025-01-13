dfb93bed3866aa6cf73e4e3020db70797c541936
Peterson Couto <142356426+petersonfernandocouto@users.noreply.github.com>
2025-01-13 17:01:05 -0300

    Explicando a logica

--- Componentes/Form.tsx
+++ Componentes/Form.tsx
@@ -7,6 +7,16 @@ const Form = () => {
     message: '',
   });
 
+    {/*Explicação da logica:
+
+    (e) = Responsável por capturar o evento de alteração no formulário (ex: quando o usuário digita ou apaga algo).
+
+    const { id, value } = O id é responsável por identificar os campos do formData, como name, game, etc. O value é responsável por armazenar o que o usuário digitou no formulário.
+
+    e.target = Neste caso, podemos afirmar que o 'e.target' representa o input(formulario) que está sendo alterado pelo usuário.
+    
+    */}
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     setFormData((prevState) => ({
@@ -19,16 +29,20 @@ const Form = () => {
     e.preventDefault();
     const { name, game, message } = formData;
     const whatsappURL = `https://api.whatsapp.com/send/?phone=5519991955915&text=${encodeURIComponent(
-      `Nome: ${name}\nJogo: ${game}\nMensagem: ${message}`
+      `Olá Peterson! \Nome: ${name}\nJogo: ${game}\nMensagem: ${message}`
     )}&type=phone_number&app_absent=0`;
 
     window.location.href = whatsappURL;
   };
 
   return (
+
     <div className='bg-[#000E29] flex flex-col justify-center rounded-[20px] lg:w-[500px] w-[315px] lg:h-[450px] 2xl:w-[600px] 2xl:h-[550px]'>
+
       <h1 className='lg:text-[28px] text-[25px] text-center font-bold pt-14 pb-7'>Quais jogos Busca?</h1>
+
       <form className='flex flex-col gap-4 items-center' onSubmit={handleSubmit}>
+
         <input
           id="name"
           type="text"
@@ -37,6 +51,8 @@ const Form = () => {
           value={formData.name}
           onChange={handleChange}
         />
+
+
         <input
           id="game"
           type="text"
@@ -45,18 +61,25 @@ const Form = () => {
           value={formData.game}
           onChange={handleChange}
         />
+
+
         <input
           id="message"
           type="text"
-          placeholder="Mensagem"
+          placeholder="Mensagem (Não obrigatório)"
           className="bg-white text-black w-[90%] h-[40px] rounded-[10px] p-4"
           value={formData.message}
           onChange={handleChange}
         />
-        <button className='bg-[#F75922] lg:my-3 lg:hover:-translate-y-2 transition-transform duration-700 hover:text-[#000E29] font-bold w-[90%] h-[50px] rounded-[10px]' type="submit">
+
+
+        <button className='bg-[#F75922] my-10 lg:my-3 lg:hover:-translate-y-2 transition-transform duration-700 hover:text-[#000E29] font-bold w-[90%] h-[50px] rounded-[10px]' type="submit">
           Enviar
         </button>
+
+
       </form>
+
     </div>
   );
 };
